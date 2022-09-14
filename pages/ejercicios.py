99046b93481f1e505b6fa86e1e569ac3c595e74b
Schettino <edu.schettino98@gmail.com>
2022-09-14 17:37:26 -0300

    se utiliza scss, se separan las hojas de estilos, se agrega map,mixin, each  y bucles

---
+++ pages/ejercicios.py
@@ -0,0 +1,13 @@
+from ast import For
+from platform import python_branch
+
+
+print("Ingresa un numero: ")
+numero = int(input())
+print("Determinando si es un numero triangular...")
+prueba()
+
+
+def prueba(numero):
+    resultado= 8*numero+1
+    print(resultado)
